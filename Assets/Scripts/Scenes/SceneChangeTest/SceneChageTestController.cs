3de8a51e2692c0c974c75f71fe6cf76720014eae
BiwaCoder <omi10biwa@funnystep.sakura.ne.jp>
2017-07-08 23:04:11 +0900

    ローディング処理いれてみた
    ローディング中にパネルを出して、シーンロード終わってから遷移させる

--- Assets/Scripts/Scenes/SceneChangeTest/SceneChageTestController.cs
+++ Assets/Scripts/Scenes/SceneChangeTest/SceneChageTestController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChageTestController : MonoBehaviour {
-
+	public GameObject LoadingPanel;
 	// Use this for initialization
 	void Start () {
 		
 	}
 
 	public void OnClickSceneChange(){
-		SceneManager.LoadScene ("MVC", LoadSceneMode.Single);
+		StartCoroutine(LoadScene("MVC"));
+	}
+
+	private IEnumerator LoadScene(string sceneName)
+	{		
+		LoadingPanel.SetActive (true);
+		float start = Time.realtimeSinceStartup;
+
+		AsyncOperation loadReq = SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Single);
+		loadReq.allowSceneActivation = false;
+		while (Time.realtimeSinceStartup - start < 1) {
+			yield return null;
+		}
+		loadReq.allowSceneActivation = true;
+		LoadingPanel.SetActive (false);
+
 	}
 	
 	// Update is called once per frame
